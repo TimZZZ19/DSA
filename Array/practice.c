40a82da752dfd298e4025a648d304456ad98291b
Tim <tim.email@example.com>
2025-02-01 22:05:41 +0800

    added array content

---
+++ Array/practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    // Hard-coded array
+    int arr[8] = {11, 22, 98, 234, 22, 12, 46, 9};
+
+    // 1. Display the array elements
+
+    int i = 0;
+    while (i < 8)
+    {
+        printf("arr[%d] = %d \n", i, arr[i]);
+
+        i++;
+    }
+
+    // 2. Calculate the sum and average
+
+    // 3. Find the maximum and minimum value in the array
+
+    // 4. Search for a specific element
+
+    // 5. Reverse the array
+}
